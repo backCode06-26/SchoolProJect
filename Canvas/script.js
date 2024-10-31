4ce4b9e0c43ab37b327e55d39f167325719739b1
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2024-10-31 16:38:07 +0900

    Add files via upload

--- Canvas/script.js
+++ Canvas/script.js
@@ -8,59 +8,75 @@ const colorPicker = document.querySelector('input[name="color"]');
 const brushSize = document.querySelector('input[name="range"]');
 const brushStyle = document.querySelector('select[name="select"]');
 
-
-// canvas 크기 설정, 
-// 설정안하면 마우스끝이 아닌 다른 곳에 그림을 그려집니다.
+// canvas 크기 설정
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
+let canvasStates = [];
+let isDrawing = false; // 변수 초기화
 
 // 색상, 크기, 스타일 설정
 colorPicker.addEventListener('input', () => {
-    console.log(ctx.strokeStyle);
     ctx.strokeStyle = colorPicker.value;
 });
 
 brushSize.addEventListener('input', () => {
-    console.log(ctx.lineWidth);
     ctx.lineWidth = brushSize.value;
 });
 
 brushStyle.addEventListener('input', () => {
-    console.log(ctx.lineCap);
     ctx.lineCap = brushStyle.value;
 });
 
+function saveState() {
+    canvasStates.push(canvas.toDataURL());
+}
+
 // 선 그리기 시작하는 부분
 canvas.addEventListener('mousedown', (e) => {
+    saveState();
     isDrawing = true;
     ctx.beginPath();
     ctx.moveTo(e.offsetX, e.offsetY);
+
 });
 
-// 선 그리기 끝나는 부분, 선을 이어서 그러줌
+// 선 그리기
 canvas.addEventListener('mousemove', (e) => {
     if (!isDrawing) return;
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
 });
 
-// 마우스 클릭해제, 마우스가 설정한 부분에서 나갈때
-// 그림을 그리지 못하게 제약
+// 마우스 클릭 해제
 canvas.addEventListener('mouseup', () => {
     isDrawing = false;
 });
 
 canvas.addEventListener('mouseout', () => {
     isDrawing = false;
-})
+});
 
-document.getElementById('sava').addEventListener("click", () => {
+// 저장하기
+document.getElementById('save').addEventListener("click", () => {
     const image = canvas.toDataURL();
     const link = document.createElement("a");
     link.href = image;
     link.download = "MyDesignSocks";
     link.click();
 });
+
+document.getElementById('back').addEventListener('click', () => {
+    if (canvasStates.length > 0) {
+        const img = new Image();
+        const lastState = canvasStates.pop();
+        img.src = lastState;
+        img.onload = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0);
+        };
+    }
+});
+
 
 // 초기화
 document.getElementById('clear').addEventListener('click', () => {
